5dda3787fdac1da09a75f732ca1a16f472779a8d
Самудинова <53327079+azica@users.noreply.github.com>
2024-07-01 23:37:58 +0600

    fixed emotion

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-    esbuild: {
-        jsxFactory: 'jsx',
-        jsxInject: `import { jsx } from '@emotion/react'`,
-    },
     plugins: [
         svgr({
             include: "**/*.svg",
